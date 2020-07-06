2e08fc573993208320c50b47b8574dce5a45752d
febwinter <skyv2080@gmail.com>
2020-07-06 13:02:42 +0900

    kubernetes api package included

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000; // port setting
 
-app.get('/', (req,res,next) => {
-    res.send('hello world!');
+//ejs setting
+app.set('view engine', 'ejs');
+app.set('views','views');
+
+//rendering setting
+app.get('/', (req,res) => {
+
+    res.render('aaa',{title: "title"});
+
 });
 
+//port listening setting
 app.listen(port,() => {
     console.log(`Server is running at ${port}`);
-})
+});
 
 //kubernetes API setting
 
 const kube = require('@kubernetes/client-node');
 const kc = new kube.KubeConfig();
 kc.loadFromDefault();
+
+const v1 = kc.makeApiClient(kube.CoreApi);
+
+var data1 = v1.listNode('default').then((res)=> res.body);
+var data2 = v1.listPodForAllNamespaces('default').then((res)=>res.body);
+
+var data = Promise.all([data1, data2]).then((res) => {
+    Node = res[0];
+    Pods = res[1];
+
+    var dataTotal = new Object();
+    var nodeList = new Array();
+    var lin
+})
